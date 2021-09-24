c27bcdf68dabd1ead1db8b13b395454aa199e0a3
Adrien Benete <adrbenete@gmail.com>
2021-09-24 09:38:16 +0900

    ajouut de la mention defer dans le link de bootstrap

--- includes/form_1.php
+++ includes/form_1.php
@@ -3,14 +3,11 @@
 	{
 		return false;
 	}
-	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$input_504 = $_POST['input_504'];
 	$message = $_POST['message'];
-	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
-
 	// Create email	
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
@@ -18,7 +15,6 @@
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: $input_504";	
-	
 	mail($to,$email_subject,$email_body,$headers); // Post message
 	return true;			
 ?>
